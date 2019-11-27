d8f8b695e3581731c366aeca8f55433a902b214a
Michael Sarfati <msarfati@nymi.com>
2019-11-27 14:58:14 -0500

    only scrape toplevel deps, output as csv

--- src/list.rs
+++ src/list.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 
 use cargo::core::Package;
 use cargo::CargoResult;
-use itertools::Itertools;
 
 use crate::licensed::Licensed;
 use crate::options::By;
@@ -19,22 +18,19 @@ pub fn run(mut packages: Vec<Package>, by: By) -> CargoResult<()> {
                     .push(package);
             }
 
-            license_to_packages
-                .iter()
-                .sorted_by_key(|&(license, _)| license)
-                .for_each(|(license, packages)| {
-                    let packages = packages
-                        .iter()
-                        .map(|package| package.name())
-                        .sorted()
-                        .join(", ");
-                    println!("{}: {}", license, packages);
-                })
+            let mut license_to_packages = license_to_packages.iter().collect::<Vec<_>>();
+            license_to_packages.sort_by_key(|&(license, _)| license);
+
+            for (license, packages) in license_to_packages {
+                let packages = packages.iter().map(|package| package.name().as_str()).collect::<Vec<&str>>().join(", ");
+                println!("{}: {}", license, packages);
+            }
         }
         By::Crate => {
+            println!("library,version,license,url");
             packages.sort_by_key(|package| package.name().to_owned());
             for package in packages {
-                println!("{}: {}", package.name(), package.license());
+                println!("{},{},{},https://crates.io/crates/{}", package.name(), package.version(), package.license(), package.name());
             }
         }
     }
--- src/load.rs
+++ src/load.rs
@@ -1,20 +1,26 @@
 use std::collections::HashSet;
 
 use cargo::core::dependency::Kind;
-use cargo::core::{Package, Workspace};
+use cargo::core::{ Package, Workspace };
 use cargo::ops;
 use cargo::util::important_paths::find_root_manifest_for_wd;
-use cargo::{CargoResult, Config};
+use cargo::{ Config, CargoResult };
 
 use crate::options::SelectedPackage;
 
-pub fn resolve_roots(config: &Config, package: SelectedPackage) -> CargoResult<Vec<Package>> {
+pub fn resolve_roots(
+        config: &Config,
+        package: SelectedPackage) -> CargoResult<Vec<Package>> {
     let root_manifest = find_root_manifest_for_wd(config.cwd())?;
     let workspace = Workspace::new(&root_manifest, config)?;
 
     Ok(match package {
-        SelectedPackage::All => workspace.members().cloned().collect(),
-        SelectedPackage::Default => vec![workspace.current()?.clone()],
+        SelectedPackage::All => {
+            workspace.members().cloned().collect()
+        }
+        SelectedPackage::Default => {
+            vec![workspace.current()?.clone()]
+        }
         SelectedPackage::Specific(spec) => {
             let (packages, _) = ops::resolve_ws(&workspace)?;
             let package_id = spec.query(packages.package_ids())?;
@@ -23,21 +29,17 @@ pub fn resolve_roots(config: &Config, package: SelectedPackage) -> CargoResult<V
     })
 }
 
-pub fn resolve_packages<'a, I: IntoIterator<Item = &'a Package>>(
-    config: &Config,
-    roots: I,
-) -> CargoResult<Vec<Package>> {
+pub fn resolve_packages<'a, I: IntoIterator<Item=&'a Package>>(
+        config: &Config,
+        roots: I) -> CargoResult<Vec<Package>> {
     let root_manifest = find_root_manifest_for_wd(config.cwd())?;
     let workspace = Workspace::new(&root_manifest, config)?;
 
     let (packages, resolve) = ops::resolve_ws(&workspace)?;
 
     let mut result = HashSet::new();
-    let mut to_check = roots
-        .into_iter()
-        .map(|p| p.package_id())
-        .collect::<Vec<_>>();
-    while let Some(id) = to_check.pop() {
+    let mut to_check = roots.into_iter().map(|p| p.package_id()).collect::<Vec<_>>();
+    if let Some(id) = to_check.pop() {
         if let Ok(package) = packages.get_one(id) {
             if result.insert(package) {
                 let deps = resolve.deps_not_replaced(id);
@@ -48,6 +50,23 @@ pub fn resolve_packages<'a, I: IntoIterator<Item = &'a Package>>(
                     if let Kind::Normal = dep.kind() {
                         let dep_id = resolve.replacement(dep_id).unwrap_or(dep_id);
                         to_check.push(dep_id);
+                    }
+                }
+            }
+        }
+    }
+
+    while let Some(id) = to_check.pop() {
+        if let Ok(package) = packages.get_one(id) {
+            if result.insert(package) {
+                let deps = resolve.deps_not_replaced(id);
+                for dep_id in deps {
+                    let dep = package.dependencies().iter()
+                        .find(|d| d.matches_id(dep_id))
+                        .unwrap_or_else(|| panic!("Looking up a packages dependency in the package failed, failed to find '{}' in '{}'", dep_id, id));
+                    if let Kind::Normal = dep.kind() {
+                        let dep_id = resolve.replacement(dep_id).unwrap_or(dep_id);
+                        //to_check.push(dep_id);
                     }
                 }
             }
